d963b62c2e4a3b058e81afbbe91cb91e26960582
Ekoramadhan13 <enugraha411@gmail.com>
2025-12-18 17:40:37 +0700

    menambahkan file DestinasiEntry.kt dan isi nya

---
+++ app/src/main/java/com/example/tugas9/uicontroller/route/DestinasiDetail.kt
@@ -0,0 +1,2 @@
+package com.example.tugas9.uicontroller.route
+
---
+++ app/src/main/java/com/example/tugas9/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.tugas9.uicontroller.route
+
+import com.example.tugas9.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
