d79ad63c5819931ffd71b1d35b3c273e8cb315ef
Ekoramadhan13 <enugraha411@gmail.com>
2025-12-18 16:10:53 +0700

    menambahkan ServiceApiSiswa.kt menambhakan @POST

--- app/src/main/java/com/example/tugas9/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/tugas9/apiservice/ServiceApiSiswa.kt
@@ -10,5 +10,6 @@ interface ServiceApiSiswa {
     @GET(value = "bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
-
+    @POST (value = "insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
