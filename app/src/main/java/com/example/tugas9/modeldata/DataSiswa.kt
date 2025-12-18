5c277afcbea69eba12695c0e3370284b05c0c675
Ekoramadhan13 <enugraha411@gmail.com>
2025-12-18 15:55:56 +0700

    menambahkan fun DataSiswa.toDetailSiswa

--- app/src/main/java/com/example/tugas9/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/tugas9/modeldata/DataSiswa.kt
@@ -33,3 +33,10 @@ fun DataSiswa.toUIStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UISt
     detailSiswa = this.toDetailSiswa(),
     isEntryValid = isEntryValid
 )
+
+fun DataSiswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
