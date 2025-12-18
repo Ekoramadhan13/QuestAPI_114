ffab015c8f1ff5a34f02e8c3a90ee2d8c272e4a2
Ekoramadhan13 <enugraha411@gmail.com>
2025-12-18 16:10:37 +0700

    menambahkan ServiceApiSiswa.kt menambhakan @GET

--- app/src/main/java/com/example/tugas9/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/tugas9/apiservice/ServiceApiSiswa.kt
@@ -1,4 +1,14 @@
 package com.example.tugas9.apiservice
 
+import com.example.tugas9.modeldata.DataSiswa
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface ServiceApiSiswa {
+
+    @GET(value = "bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+
 }
