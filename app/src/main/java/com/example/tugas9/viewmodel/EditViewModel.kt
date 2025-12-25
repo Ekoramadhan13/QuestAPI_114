dddd6d88849376a9b0392bd34f18adb70987589d
Ekoramadhan13 <enugraha411@gmail.com>
2025-12-25 23:38:38 +0700

    perbaikan isi file di EditViewModel.kt

--- app/src/main/java/com/example/tugas9/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/tugas9/viewmodel/EditViewModel.kt
@@ -1,31 +1,33 @@
 package com.example.tugas9.viewmodel
 
-
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.tugas9.modeldata.DetailSiswa
 import com.example.tugas9.modeldata.UIStateSiswa
 import com.example.tugas9.modeldata.toDataSiswa
-import com.example.tugas9.modeldata.toUIStateSiswa
+import com.example.tugas9.modeldata.toUiStateSiswa
 import com.example.tugas9.repositori.RepositoryDataSiswa
 import com.example.tugas9.uicontroller.route.DestinasiDetail
 import kotlinx.coroutines.launch
 import retrofit2.Response
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import com.example.tugas9.uicontroller.route.DestinasiEdit
+
 
 class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
-RepositoryDataSiswa): ViewModel() {
+RepositoryDataSiswa
+): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
-    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiEdit.itemIdArg])
     init {
         viewModelScope.launch {
             uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
-                .toUIStateSiswa(true)
+                .toUiStateSiswa(true)
         }
     }
     fun updateUiState(detailSiswa: DetailSiswa) {
