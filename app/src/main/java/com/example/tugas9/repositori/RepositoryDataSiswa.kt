6b13ce951c3b6c876bf0ec47d568e285b4e491af
Ekoramadhan13 <enugraha411@gmail.com>
2025-12-25 23:37:20 +0700

    perbaikan isi file di RepositoryDataSiswa.kt

--- app/src/main/java/com/example/tugas9/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/tugas9/repositori/RepositoryDataSiswa.kt
@@ -3,17 +3,18 @@ package com.example.tugas9.repositori
 import com.example.tugas9.apiservice.ServiceApiSiswa
 import com.example.tugas9.modeldata.DataSiswa
 
+
 interface RepositoryDataSiswa {
-    suspend fun getDataSiswa() : List<DataSiswa>
-    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
-    suspend fun getSatuSiswa(id:Int) : DataSiswa
-    suspend fun editSatuSiswa(id:Int, dataSiswa: DataSiswa):retrofit2.Response<Void>
-    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id: Int) : DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void>
 }
 
-class JaringanRepositoryDataSiswa(
+class jaringanRepositoryDataSiswa (
     private val serviceApiSiswa: ServiceApiSiswa
-):RepositoryDataSiswa{
+): RepositoryDataSiswa{
     override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void> =
         serviceApiSiswa.postSiswa(dataSiswa)
