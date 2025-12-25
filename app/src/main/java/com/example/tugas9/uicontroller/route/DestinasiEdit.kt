098b9f24783ed30b5049faae7f7a3654c487c6bd
Ekoramadhan13 <enugraha411@gmail.com>
2025-12-25 23:38:14 +0700

    perbaikan isi file di DestinasiEdit.kt

--- app/src/main/java/com/example/tugas9/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/tugas9/uicontroller/route/DestinasiEdit.kt
@@ -1,2 +1,11 @@
 package com.example.tugas9.uicontroller.route
 
+import com.example.tugas9.R
+
+object DestinasiEdit: DestinasiNavigasi {
+
+    override val route = "edit_detail"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idsiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
