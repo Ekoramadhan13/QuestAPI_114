203b4745f419b351475d2de4252a2d70be98d835
Ekoramadhan13 <enugraha411@gmail.com>
2025-12-25 23:38:00 +0700

    perbaikan isi file di DestinasiDetail.kt

--- app/src/main/java/com/example/tugas9/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/tugas9/uicontroller/route/DestinasiDetail.kt
@@ -5,4 +5,7 @@ import com.example.tugas9.R
 object DestinasiDetail : DestinasiNavigasi {
     override val route = "item_detail"
     override val titleRes = R.string.detail_siswa
+
+    const val itemIdArg = "idsiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
